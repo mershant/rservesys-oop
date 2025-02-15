cafd408cac3fcc248f464c2f72340be085a54bfc
mershant <wishthehish@gmail.com>
2025-02-15 10:50:42 +0800

    capacity display + reservation dropdown

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 from reservations import ReservationSystem
 
 app = Flask(__name__)
@@ -6,7 +6,6 @@ system = ReservationSystem()
 
 @app.route('/')
 def index():
-    # Show a form to make reservations
     return render_template('index.html')
 
 @app.route('/make_reservation', methods=['POST'])
@@ -14,23 +13,8 @@ def make_reservation():
     name = request.form.get('customerName')
     resource = request.form.get('resourceId')
     date_str = request.form.get('reservationDate')
-
     success, message = system.make_reservation(name, resource, date_str)
-    if success:
-        return render_template('success.html', message=message)
-    else:
-        return render_template('success.html', message=message)
-
-@app.route('/list_reservations', methods=['GET', 'POST'])
-def list_reservations():
-    if request.method == 'POST':
-        date_str = request.form.get('listDate')
-        reservations = system.list_reservations_for_date(date_str)
-        return render_template('reservations.html',
-                               date_str=date_str,
-                               reservations=reservations)
-    else:
-        return render_template('reservations.html', date_str=None, reservations=[])
+    return render_template('success.html', message=message)
 
 @app.route('/cancel_reservation', methods=['POST'])
 def cancel_reservation():
@@ -40,17 +24,31 @@ def cancel_reservation():
     success, message = system.cancel_reservation(name, resource, date_str)
     return render_template('success.html', message=message)
 
-@app.route('/capacity', methods=['GET', 'POST'])
+@app.route('/set_capacity', methods=['GET', 'POST'])
 def set_capacity():
     if request.method == 'POST':
         date_str = request.form.get('capacityDate')
-        capacity = int(request.form.get('capacityValue'))
-        system.set_capacity_for_date(date_str, capacity)
-        return render_template('success.html',
-                               message=f"Capacity for {date_str} set to {capacity}")
+        capacity_value = int(request.form.get('capacityValue'))
+        system.set_capacity_for_date(date_str, capacity_value)
+        return render_template('success.html', message=f"Capacity for {date_str} set to {capacity_value}")
     else:
         return render_template('capacity.html')
 
+@app.route('/list_reservations', methods=['GET', 'POST'])
+def list_reservations():
+    if request.method == 'POST':
+        chosen_date = request.form.get('selectedDate')
+        all_for_date = system.list_reservations_for_date(chosen_date)
+        # Get capacity for chosen_date; defaulting to 10 if not set
+        capacity = system.capacity_by_date.get(chosen_date, 10)
+        return render_template('reservations.html',
+                               date_str=chosen_date,
+                               reservations=all_for_date,
+                               capacity=capacity)
+    else:
+        date_keys = list(system.reservations_by_date.keys())
+        return render_template('list_dropdown.html', date_keys=date_keys)
+
+
 if __name__ == '__main__':
-    # Run the Flask application
     app.run(debug=True)
